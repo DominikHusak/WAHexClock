68ba6b175a7cffc733b9a5ee512d7baae0388a8b
Dominik Husák <domca.husak@gmail.com>
2023-01-10 16:23:50 +0100

    third commit

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ function Time(){
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
 
+    if(10 >= hours){
+        hours = "0" + hours;
+    }
+
+    if(10 >= minutes){
+        minutes = "0" + minutes;
+    }
+
+    if(10 >= seconds){
+        seconds = "0" + seconds;
+    }
+
     document.getElementById('time').innerHTML = "#" + hours + minutes + seconds;
+    document.body.style.backgroundColor = "#" + hours + minutes + seconds;
 }
 
 setInterval(Time, 10);
